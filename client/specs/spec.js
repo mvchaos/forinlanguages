347c99ac36f0925fbb6f55a1a7a08f49b22440d3
James Youn <jamesyoun710@gmail.com>
2016-02-26 11:15:41 -0800

    (tests) Add e2e smoke test with basic app injection

---
+++ client/specs/spec.js
@@ -0,0 +1,7 @@
+describe('Protractor Demo App', function(){
+  it('should have a title', function(){
+    // browser.get('https://sendair.herokuapp.com')
+    browser.get('http://localhost:3000')
+    expect(browser.getTitle()).toEqual('For-In Languages');
+  });
+});
