97a8529c7e3e245dcd7115f32e8e4f19bad3469a
James Youn <jamesyoun710@gmail.com>
2016-02-26 17:49:07 -0800

    (tests) Add e2e tests

--- client/specs/spec.js
+++ client/specs/spec.js
@@ -1,18 +1,23 @@
 describe('Protractor Demo App', function(){
-  it('Login successfully', function(){
+  it('Navigates to Login Page', function(){
     // browser.get('https://sendair.herokuapp.com')
     browser.get('http://localhost:3000')
 
-    // change to page header instead of title
+    //sanity test
     expect(browser.getTitle()).toEqual('For-In Languages');
 
+    var loginHeader = element(by.id('loginHeader'));
+    expect(loginHeader.elementArrayFinder_.locator_.value).toBe('loginHeader');
+
+  });
+
+  xit('Login Successfully', function(){
     element(by.model('username')).sendKeys('admin');
     element(by.model('password')).sendKeys('password1234');
+    element(by.id('loginButton')).click(); // get PR from arlen
 
-    // element(by.id('loginButton')).click(); // get PR from arlen
-
-    // element(by.id('loginHeader')).then(function(attr){
-    //   console.log(attr);
-    // });
+    var mainPageHeader = element(by.id('mainView'));
+    expect(mainPageHeader.elementArrayFinder_.locator_.value).toBe('For-In-Languages');
+mainView
   });
 });
