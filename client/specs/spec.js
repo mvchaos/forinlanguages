73a235a5793c97557b9c00533b726c78e4681e2b
James Youn <jamesyoun710@gmail.com>
2016-02-26 19:15:02 -0800

    (test)Add signup e2e test

--- client/specs/spec.js
+++ client/specs/spec.js
@@ -8,16 +8,29 @@ describe('Protractor Demo App', function(){
 
     var loginHeader = element(by.id('loginHeader'));
     expect(loginHeader.elementArrayFinder_.locator_.value).toBe('loginHeader');
-
   });
 
-  xit('Login Successfully', function(){
+  it('Login Successfully', function(){
+    browser.get('http://localhost:3000')
+
     element(by.model('username')).sendKeys('admin');
     element(by.model('password')).sendKeys('password1234');
-    element(by.id('loginButton')).click(); // get PR from arlen
+    element(by.id('loginButton')).click();
 
     var mainPageHeader = element(by.id('mainView'));
     expect(mainPageHeader.elementArrayFinder_.locator_.value).toBe('For-In-Languages');
-mainView
+  });
+
+  it('Signup Successfully', function(){
+    browser.get('http://localhost:3000/#/signup');
+
+    element(by.model('username')).sendKeys('admin');
+    element(by.model('passwordOne')).sendKeys('password1234');
+    element(by.model('passwordTwo')).sendKeys('password1234');
+    element(by.model('email')).sendKeys('admin@sendair.com');
+    element(by.id('signupButton')).click();
+
+    var mainPageHeader = element(by.id('mainView'));
+    expect(mainPageHeader.elementArrayFinder_.locator_.value).toBe('For-In-Languages');
   });
 });
