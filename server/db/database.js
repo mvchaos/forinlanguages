677e620b2d20639c75483e8eed4b7d8a6e957159
Darko <darkogjorgoski@gmail.com>
2016-02-27 11:33:13 -0800

    new connect db

--- server/db/database.js
+++ server/db/database.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "fileData"
+  host: "us-cdbr-iron-east-03.cleardb.net" || process.env.DB_HOST,
+  user: "bcec4e36a8041f",
+  password: "6bc9a6f7",
+  database: "heroku_cbbeaa791774cef"
 });
 
 connection.connect(function(err) {
@@ -15,12 +15,12 @@ if (err) {
 console.log("connected");
 });
 
-// connection.query('SELECT * from users', function(err,rows) {
-//     if (err) throw err;
+connection.query('SELECT * from users', function(err,rows) {
+    if (err) throw err;
 
-//     console.log('Data received:');
-//     console.log(rows[0].username);
-// });
+    console.log('Data received:');
+    console.log(rows[0].username);
+});
 
 
 module.exports = connection;
