288e9e1ad786c9664f24680d03de24b5abb87d13
Arlen Neylon <arlen.m.neylon@gmail.com>
2016-02-29 10:38:56 -0800

    (interface) set up for simple chart.

--- client/app/stats/stats.js
+++ client/app/stats/stats.js
@@ -1,5 +1,20 @@
-angular.module('forinlanguages.stats', [])
+angular.module('forinlanguages.stats', ['chart.js'])
 
 .controller('statsController', function($scope) {
   $scope.test = 'stats'
+/*
+  $scope.randomScalingFactor = function(){
+    return Math.round(Math.random()*100)
+  };
+  $scope.testMessage = " this is all of the stats ";
+
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  $scope.series = ['Series A', 'Series B'];
+
+  $scope.data = [
+    [$scope.randomScalingFactor(), $scope.randomScalingFactor(), $scope.randomScalingFactor(), $scope.randomScalingFactor(), $scope.randomScalingFactor(), $scope.randomScalingFactor(), $scope.randomScalingFactor()],
+    [$scope.randomScalingFactor(), $scope.randomScalingFactor(), $scope.randomScalingFactor(), $scope.randomScalingFactor(), $scope.randomScalingFactor(), $scope.randomScalingFactor(), $scope.randomScalingFactor()]
+  ];
+  $scope.onClick = function (points, evt) {
+  };*/
 })
