5efe574a62868be18b5ba491c6d7f1613f0a577c
Dan Novograd <dnovograd@gmail.com>
2016-02-29 18:11:41 -0800

    client-side validation for signup

--- client/app/signup/signup.js
+++ client/app/signup/signup.js
@@ -5,15 +5,17 @@ angular.module('forinlanguages.signup', [])
   $scope.alreadyRegistered = false;
   $scope.passMatch = false;
   $scope.newUser = {};
+  $scope.badLength = false;
   $scope.signup = function() {
     //handle new user submit
     if ($scope.newUser.signupPW !== $scope.passwordTwo) {
       $scope.passMatch = true;
     }
-    if ($scope.newUser.signupUN.length < 4 || $scope.newUser.signupPW.length < 5) {
-      console.log('nah');
+    if ($scope.newUser.signupPW.length > 15 || $scope.newUser.signupPW.length < 5) {
+      $scope.badLength = true;
     }
     else {
+    $scope.badLength = true;
     $scope.passMatch = false;
     return $http({
       method: 'POST',
